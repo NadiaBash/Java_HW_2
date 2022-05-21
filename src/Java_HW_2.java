4fc1614913ce46edfd6377e2635f39b31c71a74a
Nadia Bash <pretty_belka_2018@bk.ru>
2022-05-21 19:36:06 +0500

    Initial commit

---
+++ src/Java_HW_2.java
@@ -0,0 +1,20 @@
+import java.sql.SQLOutput;
+
+public class Java_HW_2 {
+
+    public static void main(String[] args) {
+        int amount = 80_000;
+        int limit = 1;
+        int spentMoney =20;
+
+        int bonus = amount / spentMoney * limit;
+
+        System.out.println(bonus);
+
+    }
+
+}
+
+
+
+
